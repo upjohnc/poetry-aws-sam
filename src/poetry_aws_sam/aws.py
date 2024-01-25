ec23fbb770683c9c511d3368c2dcc8b32c22a30d
Chad Upjohn <chad.upjohn@ameritas.com>
2024-01-25 15:51:42 -0500

    Working build_lambda - creates dependencies in aws bulid dir

---
+++ src/poetry_aws_sam/aws.py
@@ -0,0 +1,58 @@
+from dataclasses import dataclass
+from os import sep
+from pathlib import Path
+from subprocess import run  # nosec
+from typing import Dict, List, Optional
+
+import yaml
+
+
+@dataclass
+class AwsLambda:
+    name: str
+    path: Path
+
+
+class Sam:  # pylint: disable=too-few-public-methods
+    def __init__(self, sam_exec: str, template: Path):
+        self.exec = sam_exec
+        self.template_path = template
+        self.template = self._parse_sam_template()
+        self.lambdas = self._get_aws_lambdas()
+
+    def _get_aws_lambdas(self) -> List[AwsLambda]:
+        resources = self.template["Resources"]
+        lambdas = {
+            resource: {
+                **self.template.get("Globals", {}).get("Function", {}),
+                **resources[resource]["Properties"],
+            }
+            for resource, param in resources.items()
+            if param["Type"] == "AWS::Serverless::Function"
+        }
+        return [
+            AwsLambda(
+                name=resource,
+                path=Path(param["Handler"].replace(".", sep)).parent.parent,
+            )
+            for resource, param in lambdas.items()
+            if param.get("Runtime", "").lower().startswith("python")
+        ]
+
+    def _parse_sam_template(self) -> Dict:
+        yaml.SafeLoader.add_multi_constructor("!", lambda *args: None)
+        return yaml.safe_load(self.template_path.read_text(encoding="utf-8"))
+
+    def invoke_sam_build(self, build_dir: str, params: Optional[List[str]] = None):
+        def_params = ["--template", str(self.template_path), "--build-dir", build_dir]
+        if not params:
+            params = []
+        params.extend(def_params)
+
+        return run(
+            [self.exec, "build"] + params,
+            text=True,
+            encoding="utf-8",
+            capture_output=True,
+            check=False,
+        )
---
+++ src/poetry_aws_sam/sam.py
@@ -0,0 +1,98 @@
+import os
+import sys
+from contextlib import suppress
+from os import sep
+from pathlib import Path
+from shlex import quote
+from shutil import copy, rmtree
+from subprocess import PIPE, check_call  # nosec
+from typing import Any, Dict, List
+
+from hatch_aws.config import AwsBuilderConfig
+from hatchling.builders.plugin.interface import BuilderInterface, IncludedFile
+from hatchling.metadata.utils import normalize_project_name
+
+from poetry_aws_sam.aws import AwsLambda, Sam
+from poetry_aws_sam.export import ExportLock
+
+
+class AwsBuilder(BuilderInterface):
+    def __init__(self, root):
+        super().__init__(root=root)
+
+    def get_version_api(self) -> Dict:
+        return {"standard": self.build_standard}
+
+    # def build_lambda(self, aws_lambda: AwsLambda=None, shared_files: List[IncludedFile]=None) -> None:
+    def build_lambda(self, aws_lambda: AwsLambda = None) -> None:
+        # build_dir = Path(self.config.directory) / aws_lambda.name
+        build_dir = Path.cwd() / Path(".aws-sam/build/nelly/")
+        target = build_dir / "poetry_aws_sam"
+        requirements_file = target / "requirements.txt"
+        requirements_file.parent.mkdir(exist_ok=True, parents=True)
+        ExportLock().handle()
+
+        # requirements_file.write_text(data="\n".join(deps), encoding="utf-8")
+        check_call(
+            [
+                sys.executable,
+                "-m",
+                "pip",
+                "install",
+                "--upgrade",
+                "--disable-pip-version-check",
+                "--no-python-version-warning",
+                "-r",
+                quote(str(requirements_file)),
+                "-t",
+                quote(str(build_dir)),
+            ],
+            stdout=PIPE,
+            stderr=PIPE,
+            shell=False,
+        )
+
+    def build_standard(self, directory: str, **_build_data) -> str:
+        self.config: AwsBuilderConfig
+        try:
+            sam = Sam(sam_exec=self.config.sam_exec, template=self.config.template)
+        except AttributeError:
+            self.app.abort(
+                "Unsupported type for a 'CodeUri' or 'Handler'. Only string is supported."
+                "Functions !Sub, !Ref and others are not supported yet. "
+            )
+            raise
+        self.app.display_waiting("Building lambda functions ...")
+        result = sam.invoke_sam_build(build_dir=self.config.directory, params=self.config.sam_params)
+        if result.returncode != 0:
+            self.app.display_error(result.stderr)
+            self.app.abort("SAM build failed!")
+
+        if self.config.use_sam:
+            return directory
+
+        shared_files = list(self.recurse_included_files())
+        for aws_lambda in sam.lambdas:
+            self.app.display_info(f"{aws_lambda.name} ...", end=" ")
+            self.build_lambda(aws_lambda=aws_lambda, shared_files=shared_files)
+            self.app.display_success("success")
+
+        if self.config.force_include:
+            build_dir = Path(self.config.directory)
+            for file in shared_files:
+                if not "*" in file.distribution_path:
+                    copy(src=file.path, dst=build_dir / file.distribution_path)
+                    continue
+                *glob, filename = file.distribution_path.rpartition("*")
+                for path in build_dir.glob(pattern="".join(glob)):
+                    if path.is_file():
+                        continue
+                    if filename.startswith(sep):
+                        filename = filename[1:]
+                    target = path / filename
+                    target.parent.mkdir(exist_ok=True, parents=True)
+                    if not target.exists():
+                        copy(src=file.path, dst=target)
+        self.app.display_success("What is this")
+        self.app.display_success("Build successfull 🚀")
+        return directory
