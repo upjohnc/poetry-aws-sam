f0b29a59ab9b89387b5851f7dc1baa0682dc21ce
Chad Upjohn <chad.upjohn@ameritas.com>
2024-01-26 16:20:34 -0500

    Setting root dir as pyproject dir

--- src/poetry_aws_sam/main.py
+++ src/poetry_aws_sam/main.py
@@ -3,15 +3,32 @@ from pathlib import Path
 from poetry_aws_sam.aws import Config
 from poetry_aws_sam.sam import AwsBuilder
 
-# def main_cli():
-#     config = Config(
-#         root_dir=Path.cwd(),
-#         sam_exec="sam",
-#         template_name="template.yml",
-#     )
+ROOT_FILE = "pyproject.toml"
 
-#     _ = AwsBuilder(config).build_standard()
+
+def find_root_dir() -> Path:
+    present_dir = Path.cwd()
+
+    # assume that 3 levels or less
+    for _ in range(3):
+        check = [i for i in present_dir.glob("*") if ROOT_FILE in i.name]
+
+        if len(check) > 0:
+            break
+        present_dir = present_dir.parent
+
+    return present_dir
 
 
 def main_cli():
-    print(Path.cwd())
+    config = Config(
+        root_dir=find_root_dir(),
+        sam_exec="sam",
+        template_name="template.yml",
+    )
+
+    _ = AwsBuilder(config).build_standard()
+
+
+if __name__ == "__main__":
+    main_cli()
