8f82640369de8b4183ffbc19c71e684c9402d495
Chad Upjohn <chad.upjohn@ameritas.com>
2024-01-26 15:29:34 -0500

    Test out cwd

--- src/poetry_aws_sam/main.py
+++ src/poetry_aws_sam/main.py
@@ -3,12 +3,15 @@ from pathlib import Path
 from poetry_aws_sam.aws import Config
 from poetry_aws_sam.sam import AwsBuilder
 
+# def main_cli():
+#     config = Config(
+#         root_dir=Path.cwd(),
+#         sam_exec="sam",
+#         template_name="template.yml",
+#     )
+
+#     _ = AwsBuilder(config).build_standard()
+
 
 def main_cli():
-    config = Config(
-        root_dir=Path.cwd(),
-        sam_exec="sam",
-        template_name="template.yml",
-    )
-
-    _ = AwsBuilder(config).build_standard()
+    print(Path.cwd())
